414f88f0d4f0a675c2fb1f13c8eb46447a571c5d
HarshitThakur007 <55428817+HarshitThakur007@users.noreply.github.com>
2020-08-22 19:33:41 +0530

    Updates

---
+++ precache-manifest.3ab4fcb8b45b1f0d89351077240df288.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d699ad5878b0809bebc680c5595c4eca",
+    "url": "/rkassociates/legallyyours/index.html"
+  },
+  {
+    "revision": "099501dc04c25d62d6de",
+    "url": "/rkassociates/legallyyours/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d488d2f69489048b3f9e",
+    "url": "/rkassociates/legallyyours/static/js/2.65ec75c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rkassociates/legallyyours/static/js/2.65ec75c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "099501dc04c25d62d6de",
+    "url": "/rkassociates/legallyyours/static/js/main.0924ea0b.chunk.js"
+  },
+  {
+    "revision": "e80ffb08d14a511331c5",
+    "url": "/rkassociates/legallyyours/static/js/runtime-main.9d341994.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rkassociates/legallyyours/static/media/logo.5d5d9eef.svg"
+  }
+]);
